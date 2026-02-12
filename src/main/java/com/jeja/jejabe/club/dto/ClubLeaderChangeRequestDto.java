0208acfc4d1389c9b196e15fc111b5675f99f6b0
yoodab <yoondab2166@gmail.com>
2026-02-12 16:56:38 +0900

    feat: 팀장 변경 및 멤버 추가/삭제 기능 추가

---
+++ src/main/java/com/jeja/jejabe/club/dto/ClubLeaderChangeRequestDto.java
@@ -0,0 +1,10 @@
+package com.jeja.jejabe.club.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ClubLeaderChangeRequestDto {
+    private Long newLeaderId;
+}
