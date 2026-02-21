0c6ed4c4578e9787af7f3143d3cc65379ba36d8b
yoodab <yoondab2166@gmail.com>
2026-02-22 02:11:41 +0900

    feat: 설문 조회 및 제출 API 보안 예외 처리 (게스트 허용)

--- src/main/java/com/jeja/jejabe/JejaBeApplication.java
+++ src/main/java/com/jeja/jejabe/JejaBeApplication.java
@@ -1,5 +1,6 @@
 package com.jeja.jejabe;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
@@ -7,11 +8,20 @@ import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoCon
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 //@SpringBootApplication
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class})
 @EnableJpaAuditing
 @EnableScheduling
 public class JejaBeApplication {
+
+    @PostConstruct
+    public void init() {
+        // 서버 시간대를 한국 시간으로 고정 (UTC 문제 방지)
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        System.out.println("Set TimeZone to Asia/Seoul");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(JejaBeApplication.class, args);
